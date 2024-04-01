de23c4d37138dd4d8dd4f6f2b389200cd8d6d5ab
alejandropriv <alejandropriv@outlook.com>
2024-04-01 21:28:33 +0200

    chore: Encrypted credentials - installing script

--- installation.sh
+++ installation.sh
@@ -7,7 +7,7 @@ helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 
 # Seal the corresponding ArgoCD private-repo-creds.yaml
-kubeseal ./applications/argocd/private-repo-creds.unsealed.yaml -w ./applications/argocd/private-repo-creds.yaml
+kubeseal -f ./applications/argocd/private-repo-creds.unsealed.yaml -w ./applications/argocd/private-repo-creds.yaml
 
 #Run the installation of ArgoCD
 kubectl --context minik-nodes apply -k ./environments/development/
