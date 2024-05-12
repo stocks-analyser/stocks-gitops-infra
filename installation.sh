ca9bed8e419b84da4e04d8c34eb8ed950f249b98
alejandropriv <alejandropriv@outlook.com>
2024-05-12 06:36:06 +0200

    chore: argocd configuration

--- installation.sh
+++ installation.sh
@@ -6,19 +6,21 @@ kubectl config use-context minik-nodes
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 
-cd /opt/repos/stock-cluster
 
 # Create the sealed-secret with the credentials to create to Argo
 # Access to the repo
-#  /opt/repos/stock-cluster/tools/create_sealed_secret.sh private-repo-creds.unsealed.yaml
-#  mv sealed-private-repo-creds.unsealed.yaml private-repo-creds.yaml
+cd /opt/repos/stock-cluster/environments/development
+/opt/repos/stock-cluster/tools/create_sealed_secret.sh private-repo-creds.unsealed.yaml
+mv sealed-private-repo-creds.unsealed.yaml private-repo-creds.yaml
+
 
 # Seal the corresponding ArgoCD private-repo-creds.yaml
-kubeseal -f ./applications/argocd/private-repo-creds.unsealed.yaml -w ./applications/argocd/private-repo-creds.yaml
+kubeseal -f /opt/repos/stock-cluster/environments/development/argocd/private-repo-creds.unsealed.yaml \
+-w /opt/repos/stock-cluster/environments/development/argocd/private-repo-creds.yaml
 
 #Run the installation of ArgoCD
-kubectl --context minik-nodes apply -f namespace.yaml
-kubectl --context minik-nodes apply -k ./environments/development/
+kubectl --context minik-nodes apply -k /opt/repos/stock-cluster/applications/argocd
+kubectl --context minik-nodes apply -k /opt/repos/stock-cluster/environments/development
 
 # Chek the initial admin password
 kubectl get secret -n argocd argocd-initial-admin-secret -o go-template='{{range $k,$v := .data}}{{"### "}}{{$k}}{{"\n"}}{{$v|base64decode}}{{"\n\n"}}{{end}}'
