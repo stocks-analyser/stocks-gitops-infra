768e55174a05f08a6d6c3b81dce8ed3a2c3faf33
alejandropriv <alejandropriv@outlook.com>
2025-02-26 21:05:53 +0100

    chore: sealedsecret - traefik

--- installation.sh
+++ installation.sh
@@ -5,7 +5,7 @@ helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 
 #Run the installation of ArgoCD
-kubectl apply -k /Users/alejandroprieto/projects/stock-cluster/applications/argocd
+#kubectl apply -k /Users/alejandroprieto/projects/stock-cluster/applications/argocd
 kubectl apply -k /Users/alejandroprieto/projects/stock-cluster/environments/development
 
 # Chek the initial admin password
